b3a50217f66a9ba18edac100d906e986c73b5489
Vladislav <vladislav@MacBook-Air-Vladislav.local>
2019-11-23 16:16:58 +0200

    release v1

---
+++ dist/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+
+    $('.popup').popover({
+        trigger: 'hover',
+        placement: 'bottom',
+        delay: { "show": 350, "hide": 150 }
+      })
+
+      $('.collapse').collapse({
+        toggle: false
+      });
+  
+});
